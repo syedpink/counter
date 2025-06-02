fa0f89fae38131cb219724db335f0997fa832093
syedpink <syedshaina20@gmail.com>
2025-06-02 12:22:18 +0530

    first commit

---
+++ src/Practice.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Practice() {
+let numbers = [1, 2, 3, 4, 5];
+
+// remove one element
+// starting from index 2
+numbers.splice(1, 3);
+
+console.log(numbers);
+
+// Output: [ 1, 2, 4, 5 ]
+
+return(
+    <>
+    </>
+)
+
+}
+
+export default Practice
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Counter from './Counter.jsx'
-
+// import App from './App.jsx'
+// import Counter from './Counter.jsx'
+import Todo from './Todo.jsx'
+// import Password from './Password.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Counter/>
+    <Todo/>
+    {/* <Password/> */}
+    {/* <Counter/> */}
   </StrictMode>,
 )
